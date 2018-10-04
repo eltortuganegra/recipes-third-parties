e7493ea8b61d5d0e56e2e8f40306dc7666a4f1fa
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-10-04 09:21:28 +0200

    Remove comment code.

--- tests/services/searchRecipesService/SearchRecipesServiceWithMyRecipesProviderTest.php
+++ tests/services/searchRecipesService/SearchRecipesServiceWithMyRecipesProviderTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use rhp\infrastructure\recipesProvider\myRecipesProvider\MyRecipesProviderFactory;
 use rhp\services\searchRecipesService\SearchRecipesService;
 use rhp\services\searchRecipesService\SearchRecipesServiceRequestFactory;
 
